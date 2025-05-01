5e32260319cd9091b5b6269377824322c9054785
Rutuja1923 <rutujajirage19@gmail.com>
2025-05-01 14:59:59 +0530

    add columns, rename column (change schema)

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,15 @@
 import * as p from "drizzle-orm/pg-core";
 
+export const genderEnum = p.pgEnum("gender_enum", ["male", "female", "other"]);
+
 export const userTable = p.pgTable("user", {
-  id: p.uuid("id").primaryKey().defaultRandom(),
+  userId: p.uuid("user_id").primaryKey().defaultRandom(),
   firstName: p.varchar("first_name", { length: 255 }).notNull(),
   lastName: p.varchar("last_name", { length: 255 }).notNull(),
   age: p.integer().notNull(),
-  email: p.text("email"),
-  phoneNumber: p.text("phone_number"),
+  email: p.text("email").notNull().unique(),
+  phoneNumber: p.varchar("phone_number", { length: 15 }),
+  gender: genderEnum("gender").notNull(),
 });
 
 //id2: serial("id2").primaryKey() --> import serial or p.serial
