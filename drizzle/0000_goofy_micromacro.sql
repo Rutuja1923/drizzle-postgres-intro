5eac83f939f4d66c6537b3b21ce9a071cac12398
Rutuja1923 <rutujajirage19@gmail.com>
2025-05-01 10:17:10 +0530

    configure Drizzle ORM and add initial user table migration

---
+++ drizzle/0000_goofy_micromacro.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "user" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"age" integer NOT NULL
+);
