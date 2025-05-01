0956b4df5250d0b73152c2b9dcd90914984f4a41
Rutuja1923 <rutujajirage19@gmail.com>
2025-05-01 10:30:33 +0530

    alter table add new columns

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,12 @@
-import { pgTable as table, uuid, integer, varchar } from "drizzle-orm/pg-core";
+import * as p from "drizzle-orm/pg-core";
 
-export const userTable = table("user", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  firstName: varchar("first_name", { length: 255 }).notNull(),
-  lastName: varchar("last_name", { length: 255 }).notNull(),
-  age: integer().notNull(),
+export const userTable = p.pgTable("user", {
+  id: p.uuid("id").primaryKey().defaultRandom(),
+  firstName: p.varchar("first_name", { length: 255 }).notNull(),
+  lastName: p.varchar("last_name", { length: 255 }).notNull(),
+  age: p.integer().notNull(),
+  email: p.text("email"),
+  phoneNumber: p.text("phone_number"),
 });
 
-//id2: serial("id2").primaryKey() --> import serial
+//id2: serial("id2").primaryKey() --> import serial or p.serial
