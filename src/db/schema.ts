b26ee4108125632a3706e85e8df341e85ce2e12a
Rutuja1923 <rutujajirage19@gmail.com>
2025-05-01 20:07:47 +0530

    Add table user preferences (1-1 relationship)

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,29 @@
+import { boolean } from "drizzle-orm/gel-core";
 import * as p from "drizzle-orm/pg-core";
 
 export const genderEnum = p.pgEnum("gender_enum", ["male", "female", "other"]);
+export const roleEnum = p.pgEnum("role_enum", ["ADMIN", "USER"]);
 
-export const userTable = p.pgTable("user", {
+export const userTable = p.pgTable(
+  "user",
+  {
+    userId: p.uuid("user_id").primaryKey().defaultRandom(),
+    firstName: p.varchar("first_name", { length: 255 }).notNull(),
+    lastName: p.varchar("last_name", { length: 255 }).notNull(),
+    age: p.integer().notNull(),
+    email: p.text("email").notNull().unique(),
+    phoneNumber: p.varchar("phone_number", { length: 15 }),
+    gender: genderEnum("gender").notNull(),
+    role: roleEnum("role").default("USER").notNull(),
+  },
+  (table) => [p.index("email_index").on(table.email)]
+);
+
+export const userPreferencesTable = p.pgTable("userPrefereces", {
   userId: p.uuid("user_id").primaryKey().defaultRandom(),
-  firstName: p.varchar("first_name", { length: 255 }).notNull(),
-  lastName: p.varchar("last_name", { length: 255 }).notNull(),
-  age: p.integer().notNull(),
-  email: p.text("email").notNull().unique(),
-  phoneNumber: p.varchar("phone_number", { length: 15 }),
-  gender: genderEnum("gender").notNull(),
+  emailUpdates: p.boolean("email_updates").notNull().default(false),
+  userRefId: p
+    .uuid("user_ref_id")
+    .references(() => userTable.userId)
+    .notNull(),
 });
-
-//id2: serial("id2").primaryKey() --> import serial or p.serial
