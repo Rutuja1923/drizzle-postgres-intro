5f731eb520571907fafd1bbe49bbd4e8be78ed3b
Rutuja1923 <rutujajirage19@gmail.com>
2025-05-11 15:43:43 +0530

    Add the uuid of an existing user to delete

--- src/queries/removeUser.ts
+++ src/queries/removeUser.ts
@@ -1,7 +1,10 @@
 import { deleteUser } from "../db/db_services/user";
 
+//add the user id that you wish to delete
+let uId = "488a466a-4c93-4ed9-bcca-9e98cc02843f"; 
+
 const main = async () => {
-  const res = await deleteUser("488a466a-4c93-4ed9-bcca-9e98cc02843f");
+  const res = await deleteUser(uId);
   console.log(res);
 };
 
